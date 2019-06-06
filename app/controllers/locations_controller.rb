11ce01b48c52f878923e815f0dad9bb41b67033c
ryan <ryan.neill11@gmail.com>
2019-06-06 10:25:10 -0400

    old changes

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -28,7 +28,7 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       if @location.save
-        format.html { redirect_to @location, notice: 'Location was successfully created.' }
+        format.html { redirect_to locations_path, notice: 'Location was successfully created.' }
         format.json { render :show, status: :created, location: @location }
 
       else
